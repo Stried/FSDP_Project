87acb5167c1a2df820857d4fad8638a6e540500e
Stried <alanangkh@gmail.com>
2023-05-17 10:09:29 +0800

    new commit#1

---
+++ server/models/ecolifeData.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const userAccount = sequelize.define("Account Creation", {
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phoneNo: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        emailAccount: { // Unique Key?
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return userAccount;
+}
