a2679c4cc28e374f888be0dd7058afa0a8e948b5
Stried <alanangkh@gmail.com>
2023-07-04 19:56:34 +0800

    CRUD for Locations finished #64

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -12,7 +12,7 @@ router.post("/createLocation", validateToken, async (req, res) => {
         locationName: yup.string().trim().required("Please provide a location name."),
         streetName: yup.string().trim().required("Please provide a street name."),
         postalCode: yup.number().positive().integer().required("Please input a valid Postal Code."),
-        region: yup.string().trim().oneOf([ "N", "S", "E", "W"]).required("Please specify a valid region."),
+        region: yup.string().trim().oneOf(["N", "S", "E", "W"]).required("Please specify a valid region."),
         fastCharge: yup.boolean().required("Please specify if the charger is capable of FastCharge."),
         noOfChargers: yup.number().positive().integer().required("Please specify the number of chargers in the location.")
     });
@@ -36,7 +36,7 @@ router.post("/createLocation", validateToken, async (req, res) => {
 
     locationInfo = {
         LatAxis: data.LatAxis,
-        LongAxis: data.LongAxis,
+        LongAxis: data.LongAxis, 
         locationName: data.locationName,
         streetName: data.streetName,
         postalCode: data.postalCode,
@@ -77,5 +77,115 @@ router.post("/createLocation", validateToken, async (req, res) => {
         return;
     }
 })
+
+router.get("/LocationsMain", async (req, res) => {
+    const conditions = {}
+    let search = req.query.search;
+    if (search) {
+        condition[ Sequelize.Op.or ] = [
+            { locationName: { [ Sequelize.Op.like ]: `%${search}%` } },
+            { streetName: { [ Sequelize.Op.like ]: `%${search}%` } },
+            { postalCode: { [ Sequelize.Op.like ]: `%${search}%` } }
+        ]
+    }
+    
+    let locations = await Locations.findAll({
+        where: conditions,
+        order:[ ['streetName', 'DESC'] ]
+    })
+
+    res.json(locations)
+})
+
+router.put("/updateLocation", validateToken, async (req, res) => {
+    let data = req.body;
+    let validationSchema = yup.object().shape({
+        LatAxis: yup.number().required("Please specify the Latitute."),
+        LongAxis: yup.number().required("Please specify the Longitude."),
+        locationName: yup.string().trim().required("Please provide a location name."),
+        streetName: yup.string().trim().required("Please provide a street name."),
+        postalCode: yup.number().positive().integer().required("Please input a valid Postal Code."),
+        region: yup.string().trim().oneOf([ "N", "S", "E", "W" ]).required("Please specify a valid region."),
+        fastCharge: yup.boolean().required("Please specify if the charger is capable of FastCharge."),
+        noOfChargers: yup.number().positive().integer().required("Please specify the number of chargers in the location.")
+    });
+    try {
+        await validationSchema.validate(data, { abortEarly: false });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({ errors: err.errors });
+        return;
+    }
+
+    data.LatAxis = data.LatAxis;
+    data.LongAxis = data.LongAxis;
+    data.locationName = data.locationName.trim();
+    data.streetName = data.streetName.trim();
+    data.postalCode = data.postalCode;
+    data.region = data.region.trim();
+    data.fastCharge = data.fastCharge;
+    data.noOfChargers = data.noOfChargers;
+
+    let locationInfo = {
+        LatAxis: data.LatAxis,
+        LongAxis: data.LongAxis,
+        locationName: data.locationName,
+        streetName: data.streetName,
+        postalCode: data.postalCode,
+        region: data.region,
+        fastCharge: data.fastCharge,
+        noOfChargers: data.noOfChargers
+    }
+
+
+    let result = await Locations.update(locationInfo, {
+        where: { postalCode: locationInfo.postalCode }
+    });
+
+    if (result == 1) {
+        res.json({
+            message: "Location has been successfully updated.",
+        });
+    } else {
+        res.status(400).json({
+            message: `Cannot update User with id ${locationInfo.emailAccount}`,
+        });
+        return;
+    }
+
+})
+
+router.delete("/deleteLocation/:id", validateToken, async (req, res) => {
+    let location = req.params.id;
+
+    if (req.user.adminNo == null) {
+        res.status(401).json({ message: "Unauthorized action taken.\nActions, logged.\nAddress, triangulated." });
+        return;
+    }
+
+    let findLocation = await Locations.findOne({
+        where: { postalCode: location } // cloumn_name: data
+    });
+    if (!findLocation) {
+        console.log("The location you're trying to delete was not found.");
+        res.sendStatus(404);
+        return;
+    } else {
+        let deleteLocation = await Locations.destroy({
+            where: { postalCode: location }
+        });
+
+        if (deleteLocation == 1) {
+            res.json({
+                message: "Charger location successfully removed."
+            });
+        } else {
+            res.status(400).json({
+                message: `Cannot remove location with postal code ${postalCode}`
+            });
+        }
+    }
+});
+
 
 module.exports = router;
