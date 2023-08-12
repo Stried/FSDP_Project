c1cf4e7af7695ad1388668c92ad3348e42f6b3ab
IamBiDone <136313328+IamBiDone@users.noreply.github.com>
2023-08-12 18:54:17 +0800

    styled and added new store receipt graph

--- client/src/pages/Trials/trialUsers/TrialsData.jsx
+++ client/src/pages/Trials/trialUsers/TrialsData.jsx
@@ -36,19 +36,55 @@ const App = () => {const calculateAverageRating = (receiptList) => {
   
 
   const [trialReceiptList, setTrialReceiptList] = useState([]);
-
+const [storeReceiptList, setStoreReceiptList] = useState([])
   const getTrialReceipt = () => {
     http.get("/trials/getAllTrialReceiptRatings").then((res) => {
       setTrialReceiptList(res.data);
     });
   };
 
+  const getStoreReceipt = () => {
+    http.get("/store/viewStoreReceipt").then((res) => {
+      setStoreReceiptList(res.data);
+    });
+  };
 
   useEffect(() => {
     getTrialReceipt();
   }, []);
 
+  useEffect(() => {
+    getStoreReceipt();
+  }, []);
+
+
+  const calculatePopularModels = (storeReceiptList) => {
+    const modelCounts = {};
+  
+    storeReceiptList.forEach((receipt) => {
+      const modelName = receipt.carModel;
+  
+      if (!modelCounts[modelName]) {
+        modelCounts[modelName] = 0;
+      }
+  
+      modelCounts[modelName]++;
+    });
+  
+    const popularModels = Object.keys(modelCounts)
+      .map((modelName) => ({
+        modelName,
+        count: modelCounts[modelName],
+      }))
+      .sort((a, b) => b.count - a.count)
+      .slice(0, 5);
+  
+    return popularModels;
+  };
+
   const averageRatings = calculateAverageRating(trialReceiptList);
+
+  const popularModels = calculatePopularModels(storeReceiptList);
 
   const sortedTop5 = averageRatings
   .sort((a, b) => b.averageRating - a.averageRating)
@@ -58,19 +94,19 @@ const App = () => {const calculateAverageRating = (receiptList) => {
     title: {
       text: "Top 5 Average Rated Trial Cars",
       fontColor: "#22c55e",
-      fontFamily: "Arial", 
+      fontFamily: "Montserrat", 
     },
     backgroundColor: "#1F2937",
     dataPointWidth: 130,
         axisX: {
       titleFontColor: "#666", // X-axis title color
       labelFontColor: "#FFFFFF",  // X-axis labels color
-      fontFamily: "Arial", 
+      fontFamily: "Montserrat", 
     },
     axisY: {
       titleFontColor: "#666", // Y-axis title color
       labelFontColor: "#FFFFFF",  // Y-axis labels color
-      fontFamily: "Arial", 
+      fontFamily: "Montserrat", 
       minimum: 0, 
       maximum: 5, 
     },
@@ -91,18 +127,67 @@ const App = () => {const calculateAverageRating = (receiptList) => {
   };
 
  
+  const chartOptionsStore = {
+    title: {
+      text: "Top 5 Most Popular Store Car Models",
+      fontColor: "#22c55e",
+      fontFamily: "Montserrat",
+    },
+    backgroundColor: "#1F2937",
+    dataPointWidth: 130,
+    axisX: {
+      titleFontColor: "#666",
+      labelFontColor: "#FFFFFF",
+      fontFamily: "Montserrat",
+    },
+    axisY: {
+      titleFontColor: "#666",
+      labelFontColor: "#FFFFFF",
+      fontFamily: "Montserrat",
+      minimum: 0,
+    },
+    legend: {
+      fontColor: "#666",
+    },
+    data: [
+      {
+        type: "column",
+        dataPoints: popularModels.map(({ modelName, count }) => ({
+          label: modelName,
+          y: count,
+          toolTipContent: `${modelName}<br>No. of orders: ${count}`,
+        })),
+      },
+    ],
+  };
   
   return (
-    <div className="flex justify-center min-h-screen">
-    <br />
-    <div className="chart-container w-11/12 mt-10">
-      {trialReceiptList.length === 0 ? (
-        <p className="text-center text-gray-500">No data available to display for the graph.</p>
-      ) : (
-        <CanvasJSChart options={chartOptions} />
-      )}
+<div className="min-h-screen">
+      <div className="flex justify-center">
+        <div className="chart-container w-11/12 mt-10">
+          {trialReceiptList.length === 0 ? (
+            <p className="text-center text-gray-500">
+              No data available to display for the trial receipts graph.
+            </p>
+          ) : (
+            <CanvasJSChart options={chartOptions} />
+          )}
+        </div>
+      </div>
+      <br />
+      <div className="flex justify-center">
+        <div className="chart-container w-11/12 mt-10">
+          {storeReceiptList.length === 0 ? (
+            <p className="text-center text-gray-500">
+              No data available to display for the store car models graph.
+            </p>
+          ) : (
+            <CanvasJSChart options={chartOptionsStore} />
+          )}
+        </div>
+      </div>
     </div>
-  </div>
+  
   );
 };
 
