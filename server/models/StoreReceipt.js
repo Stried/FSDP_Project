912cab2e5afcc7d17ee079c1d6359de0cbf55cae
Chun Kiat <angchunkiat0305@gmail.com>
2023-08-12 11:44:10 +0800

    new table called StoreReceipt

---
+++ server/models/StoreReceipt.js
@@ -0,0 +1,66 @@
+module.exports = (sequelize, DataTypes) => {
+    const StoreReceipt = sequelize.define("StoreReceipt", {
+        carPlate: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        carReceiptId: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1
+        },
+        carBrand: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        carModel: { 
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cardNumber: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cardHolderName: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        cardExpiryDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        cvc: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        boughtBy: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userAddress: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        userPhoneNo: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        userEmailAccount: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    });
+
+    StoreReceipt.associate = (models) => { // sold by who? WHY DID U STOP WORKING
+        StoreReceipt.belongsTo(models.UserAccount, {
+            foreignKey: "emailAccount",
+            as: "buyers"
+        });
+        StoreReceipt.belongsTo(models.Store, {
+            foreignKey: "carPlateNo",
+            as: "car"
+        });
+    };
+
+    return StoreReceipt;
+}
